161b7d3b0b6b71c21762d519088d44f096fb5fd6
glidebc <glide.cti@gmail.com>
2017-07-04 18:07:26 +0800

    0704

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,111 @@
+<div class="page-sidebar-wrapper">
+    <div class="page-sidebar navbar-collapse collapse">
+        <ul class="page-sidebar-menu"
+            data-keep-expanded="false"
+            data-auto-scroll="true"
+            data-slide-speed="200">
+            @if(Auth::user()->role_id == config('quickadmin.defaultRole'))
+                <li @if(Request::path() == config('quickadmin.route').'/menu') class="active" @endif>
+                    <a href="{{ url(config('quickadmin.route').'/menu') }}">
+                        <i class="fa fa-list"></i>
+                        <span class="title">{{ trans('quickadmin::admin.partials-sidebar-menu') }}</span>
+                    </a>
+                </li>
+                <li @if(Request::path() == 'users') class="active" @endif>
+                    <a href="{{ url('users') }}">
+                        <i class="fa fa-users"></i>
+                        <span class="title">{{ trans('quickadmin::admin.partials-sidebar-users') }}</span>
+                    </a>
+                </li>
+                <li @if(Request::path() == 'roles') class="active" @endif>
+                    <a href="{{ url('roles') }}">
+                        <i class="fa fa-gavel"></i>
+                        <span class="title">{{ trans('quickadmin::admin.partials-sidebar-roles') }}</span>
+                    </a>
+                </li>
+                <li @if(Request::path() == config('quickadmin.route').'/actions') class="active" @endif>
+                    <a href="{{ url(config('quickadmin.route').'/actions') }}">
+                        <i class="fa fa-users"></i>
+                        <span class="title">{{ trans('quickadmin::admin.partials-sidebar-user-actions') }}</span>
+                    </a>
+                </li>
+            @endif
+            @foreach($menus as $menu)
+                @if($menu->menu_type != 2 && is_null($menu->parent_id))
+                    @if(Auth::user()->role->canAccessMenu($menu))
+                        <li @if(isset(explode('/',Request::path())[1]) && explode('/',Request::path())[1] == strtolower($menu->name)) class="active" @endif>
+                            <a href="{{ route(config('quickadmin.route').'.'.strtolower($menu->name).'.index') }}">
+                                <i class="fa {{ $menu->icon }}"></i>
+                                <span class="title">{{ $menu->title }}</span>
+                            </a>
+                        </li>
+                    @endif
+                @else
+                    @if(Auth::user()->role->canAccessMenu($menu) && !is_null($menu->children()->first()) && is_null($menu->parent_id))
+                        <li>
+                            <a href="#">
+                                <i class="fa {{ $menu->icon }}"></i>
+                                <span class="title">{{ $menu->title }}</span>
+                                <span class="fa arrow"></span>
+                            </a>
+                            <ul class="sub-menu">
+                                @foreach($menu['children'] as $child)
+                                    @if(Auth::user()->role->canAccessMenu($child))
+                                        <li
+                                                @if(isset(explode('/',Request::path())[1]) && explode('/',Request::path())[1] == strtolower($child->name)) class="active active-sub" @endif>
+                                            <a href="{{ route(strtolower(config('quickadmin.route').'.'.$child->name).'.index') }}">
+                                                <i class="fa {{ $child->icon }}"></i>
+                                                <span class="title">
+                                                    {{ $child->title }}
+                                                    <?php
+                                                        if($child->name == 'Publishverify') {
+                                                            $countVerify = App\EntrustFlow::where('status', 'verify')->count();
+                                                            if($countVerify > 0)
+                                                                echo '&nbsp;<span class="verify-count">'.($countVerify > 9 ? '10+' : $countVerify).'</span>';
+                                                        } else if($child->name == 'MyEntrust') {
+                                                            $entrusts = App\DataQuery::collectionOfEntrustByUser(Auth::user()->id)->get();
+                                                            $countEntrust = 0;
+                                                            foreach ($entrusts as $entrust) {
+                                                                $countOk = App\EntrustFlow::whereRaw('entrust_id='.$entrust->id.' AND status=\'ok\'')->count();
+                                                                $countReject = App\EntrustFlow::whereRaw('entrust_id='.$entrust->id.' AND status=\'reject\'')->count();
+                                                                if($countOk + $countReject > 0)
+                                                                    App\EntrustFlow::where('entrust_id', $entrust->id)->delete();
+                                                                $countEntrust += $countOk + $countReject;
+                                                            }
+                                                            if($countEntrust > 0)
+                                                                echo '&nbsp;<span class="entrust-count">'.$countEntrust.'</span>';
+                                                        }
+                                                    ?>
+                                                </span>
+                                            </a>
+                                        </li>
+                                    @endif
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endif
+                @endif
+            @endforeach
+            <li>
+                {!! Form::open(['url' => 'logout']) !!}
+                {{ csrf_field() }}
+                <button type="submit" class="logout">
+                    <i class="fa fa-sign-out fa-fw"></i>
+                    <span class="title">{{ trans('quickadmin::admin.partials-sidebar-logout') }}</span>
+                </button>
+                {!! Form::close() !!}
+            </li>
+        </ul>
+    </div>
+</div>
+
+<style type="text/css">
+    .entrust-count {
+        color: #428bca;
+        font-weight: bold;
+    }
+    .verify-count {
+        color: Red;
+        font-weight: bold;
+    }
+</style>
